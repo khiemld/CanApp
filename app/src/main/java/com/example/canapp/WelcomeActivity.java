256065437f3a0d47f3587222e0c0069e26f99629
lamhoangduyen2k2 <97307113+lamhoangduyen2k2@users.noreply.github.com>
2023-04-22 13:38:17 +0700

    login_version4

--- app/src/main/java/com/example/canapp/WelcomeActivity.java
+++ app/src/main/java/com/example/canapp/WelcomeActivity.java
@@ -2,7 +2,18 @@ package com.example.canapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +21,15 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                // Thiết lập animation cho LoginActivity
+                overridePendingTransition(R.anim.slide_up_login, R.anim.no_animation);
+            }
+        });
     }
 }
