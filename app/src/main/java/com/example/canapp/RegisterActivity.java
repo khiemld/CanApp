b23adcf2c64330d76f42e64c5594b245952f17ae
Bai1_ST5_android <nhudang1511@gmail.com>
2023-04-22 22:52:16 +0700

    "update_2"

--- app/src/main/java/com/example/canapp/RegisterActivity.java
+++ app/src/main/java/com/example/canapp/RegisterActivity.java
@@ -15,6 +15,7 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
         AnhXa();
         tv_next.setOnClickListener(new View.OnClickListener() {
             @Override
