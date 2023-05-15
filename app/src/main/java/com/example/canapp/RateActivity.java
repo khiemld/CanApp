dc87e20da355f3e86bf6bd2796cb5bfaf3add977
lamhoangduyen2k2 <97307113+lamhoangduyen2k2@users.noreply.github.com>
2023-05-16 01:02:52 +0700

    login_version13: layout Rating

--- app/src/main/java/com/example/canapp/RateActivity.java
+++ app/src/main/java/com/example/canapp/RateActivity.java
@@ -1,14 +1,53 @@
 package com.example.canapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.canapp.adapter.UserAdapter;
+import com.example.canapp.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RateActivity extends AppCompatActivity {
+
+    private RecyclerView rcv_memberRate;
+
+    private UserAdapter userAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
+
+        userAdapter = new UserAdapter(getApplicationContext());
+
+        Mapping();
+        getAllMember();
+    }
+
+    private void getAllMember() {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(), RecyclerView.VERTICAL, false);
+        rcv_memberRate.setLayoutManager(layoutManager);
+
+        userAdapter.setUser(getListUser());
+        rcv_memberRate.setAdapter(userAdapter);
+    }
+
+    private List<User> getListUser() {
+        List<User> list = new ArrayList<>();
+        list.add(new User("1", "Hoàng Vũ Trường Giang", "khiemld.0204@gmail.com", "12345678", "https://www.chuphinhsanpham.vn/wp-content/uploads/2018/09/chup-anh-cv-dep-2.jpg", "Hoàng Diệu 2", "Trưởng nhóm", "0818447234", true));
+        list.add(new User("2", "Hoàng Vũ Trường Giang", "khiemld.0204@gmail.com", "12345678", "https://www.chuphinhsanpham.vn/wp-content/uploads/2018/09/chup-anh-cv-dep-2.jpg", "Hoàng Diệu 2", "Trưởng nhóm", "0818447234", true));
+        list.add(new User("3", "Hoàng Vũ Trường Giang", "khiemld.0204@gmail.com", "12345678", "https://www.chuphinhsanpham.vn/wp-content/uploads/2018/09/chup-anh-cv-dep-2.jpg", "Hoàng Diệu 2", "Trưởng nhóm", "0818447234", true));
+        list.add(new User("4", "Hoàng Vũ Trường Giang", "khiemld.0204@gmail.com", "12345678", "https://www.chuphinhsanpham.vn/wp-content/uploads/2018/09/chup-anh-cv-dep-2.jpg", "Hoàng Diệu 2", "Trưởng nhóm", "0818447234", true));
+
+        return list;
+    }
+
+    private void Mapping() {
+        rcv_memberRate = findViewById(R.id.rv_ratememberRate);
     }
 }
---
+++ app/src/main/java/com/example/canapp/adapter/UserAdapter.java
@@ -0,0 +1,72 @@
+package com.example.canapp.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.canapp.R;
+import com.example.canapp.model.User;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder>{
+
+    private Context context;
+
+    private List<User> users;
+
+    public UserAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void setUser(List<User> list){
+        this.users = list;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_personal_rate, parent, false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        User user = users.get(position);
+        if (user == null){
+            return;
+        }
+        holder.txt_nameMember.setText(user.getName());
+        holder.txt_emailMember.setText(user.getEmail());
+        Glide.with(context).load(user.getAvatar()).into(holder.img_avatarMember);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (users != null){
+            return users.size();
+        }
+        return 0;
+    }
+
+    public class UserViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView txt_nameMember, txt_emailMember;
+
+        private ImageView img_avatarMember;
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txt_nameMember = itemView.findViewById(R.id.tv_namememberRate);
+            txt_emailMember = itemView.findViewById(R.id.tv_emailmemberRate);
+            img_avatarMember = itemView.findViewById(R.id.cr_avatarRate);
+        }
+    }
+}
